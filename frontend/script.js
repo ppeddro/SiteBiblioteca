2facbf8001a2010423c4c5a8c5569e3281a781a2
ppeddro <pedroheinrich06@gmail.com>
2025-05-20 13:05:16 -0300

    Implementa modal de login com email e senha

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,7 @@
+  function abrirModal() {
+    document.getElementById('modalLogin').style.display = 'flex';
+  }
 
-//function buscarLivro() {
-//    const termo = document.getElementById("searchInput").value;
-//    const livrosEl = document.getElementById("livros");
-  
-    // Simulação de resposta (futuramente virá do backend Spring Boot)
-//    const livros = [
- //     { id: 1, titulo: "Dom Casmurro", autor: "Machado de Assis", disponivel: false },
-    //  { id: 2, titulo: "O Hobbit", autor: "J.R.R. Tolkien", disponivel: true }
-    ];
-  
- //   const resultados = livros.filter(l => l.titulo.toLowerCase().includes(termo.toLowerCase()));
-  
-//    livrosEl.innerHTML = resultados.map(l =>
-  //    `<div class="livro">
-  //      <h3>${l.titulo}</h3>
-  //      <p>Autor: ${l.autor}</p>
-   //     <p>${l.disponivel ? "Disponível" : "Emprestado"}</p>
-  //    </div>`
-    //).join("");
+  function fecharModal() {
+    document.getElementById('modalLogin').style.display = 'none';
   }
